25085199364d11fe3b836f308d050f515c793d30
Aryan Prince <aryanprincesanty@icloud.com>
2024-07-05 11:01:46 +0300

    ✨ Create simple layout with `SafeAreaView`

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { Image, StyleSheet, Text, View } from 'react-native'
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
+
+import PlaceholderImage from './assets/images/background-image.png'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <Text style={styles.headerText}>StickerSmash</Text>
+        <Image source={PlaceholderImage} style={styles.image} />
+        <Text style={styles.footerText}>Built by Aryan</Text>
+        <StatusBar style="auto" />
+      </SafeAreaView>
+    </SafeAreaProvider>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    flexDirection: 'column',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
+    backgroundColor: '#0a0a0a',
+    gap: 16,
   },
-});
+  headerText: {
+    color: '#f5f5f5',
+    fontSize: 32,
+    fontWeight: '700',
+  },
+  footerText: {
+    color: '#a3a3a3',
+    fontSize: 16,
+    fontWeight: '500',
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+})
